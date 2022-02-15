161a1897827ea3d33d15e659cea3ebb3d56a6189
TiuShotto <levi.pires2005@gmail.com>
2022-02-15 10:00:56 -0300

    Delete `index.js`

--- index.js
+++
@@ -1,8 +0,0 @@
-import {Client} from "eris"
-const client = new Client("The bot token here")
-
-client.on("messageCreate", message => {
-    message.channel.createMessage("Hello, world!")
-})
-
-client.connect().then(console.log("Bot ready!"))
